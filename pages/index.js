50010535b9e6fdd15088aa6f53b9366e85b0bb7a
Caio Marcelo <caiomfps@gmail.com>
2021-10-27 14:20:32 -0300

    nova chamada na home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function Home(){
-    return<h4>Da um liga meu caros, patch notes do site vindo ai!</h4> 
+    return<h4>Página inicial</h4> 
 }
 
 export default Home
